85242097d9834ca3e3b0490a8a29843dc50a7abb
Nickshe11 <shelobod@gmail.com>
2023-01-15 22:22:48 +0300

    ComboBox lesson 1 DONE

--- WinAPI/Main.cpp
+++ WinAPI/Main.cpp
@@ -1,7 +1,10 @@
 ﻿//WinAPI
 #include <Windows.h>
 #include "resource.h"
- 
+
+CONST CHAR gsz_LOGIN_INVITATION[] = "Введите имя пользователя"; // gsz - dlobal string zero (именованный null terminated line)
+CONST CHAR gsz_PASSWORD_INVITATION[] = "Введите пароль";
+
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 
@@ -43,7 +46,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		| MB_ICONASTERISK		//Значок
 		| MB_DEFBUTTON3			//Кнопка по умолчанию
 		| MB_SYSTEMMODAL);		// WS_EX_TOPMOST - поверх всех окон
-	
+
 
 #endif // MCG_BOX
 
@@ -60,11 +63,38 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), (LPSTR)IDI_ICON1);
 		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+
+		//Получаем обработчики окна текстовых полей (HWND - handler to window, или обработчик окна)
+		HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+		HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
+
+		SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)gsz_LOGIN_INVITATION);
+		SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)gsz_PASSWORD_INVITATION);
+
 	}
-		break;
+	break;
 	case WM_COMMAND:	// обработка команд нажатия кнопок
 		switch (LOWORD(wParam))
 		{
+		case IDC_EDIT_LOGIN:
+		{
+			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = {};
+			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			if (HIWORD(wParam) == EN_SETFOCUS)
+			{
+				if (strcmp(sz_buffer, gsz_LOGIN_INVITATION) == 0)
+					SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)"");
+			}
+			if (HIWORD(wParam) == EN_KILLFOCUS)
+			{
+				//SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+				if (strlen(sz_buffer) == 0)
+					SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)gsz_LOGIN_INVITATION);
+			}
+		}
+		break;
 		case IDC_BUTTON_COPY:
 		{
 			CONST INT SIZE = 256;
@@ -74,7 +104,7 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)buffer);
 			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)buffer);
 		}
-			break;
+		break;
 		case IDOK:MessageBox(NULL, "Была нажата кнопка ОК", "Info", MB_OK | MB_ICONINFORMATION); break;
 		case IDCANCEL:EndDialog(hwnd, 0);
 		}
