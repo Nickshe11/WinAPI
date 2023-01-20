0af0f024d4b8d325bd6af6a80774ceda6d3167f0
Nickshe11 <shelobod@gmail.com>
2023-01-20 18:17:49 +0300

    Main Window class 2

--- MainWindow/Main.cpp
+++ MainWindow/Main.cpp
@@ -1,6 +1,33 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <Windows.h>
 #include <stdio.h>
+#define IDC_BUTTON_0		1000
+#define IDC_BUTTON_1		1001
+#define IDC_BUTTON_2		1002
+#define IDC_BUTTON_3		1003
+#define IDC_BUTTON_4		1004
+#define IDC_BUTTON_5		1005
+#define IDC_BUTTON_6		1006
+#define IDC_BUTTON_7		1007
+#define IDC_BUTTON_8		1008
+#define IDC_BUTTON_9		1009
+
+#define IDC_BUTTON_PLUS		1010
+#define IDC_BUTTON_MINUS	1011
+#define IDC_BUTTON_ASTER	1012
+#define IDC_BUTTON_SLASH	1013
+#define IDC_BUTTON_POINT	1014
+#define IDC_BUTTON_EQUAL	1015
+#define IDC_BUTTON_CLEAR	1016
+#define IDC_EDIT			1017
+
+CONST INT g_INTERVAL = 5;
+CONST INT g_BUTTON_SIZE = 50;
+CONST INT g_DISPLAY_WIDTH = 400;
+CONST INT g_DISPLAY_HEIGHT = 20;
+CONST INT g_START_X = 10;
+CONST INT g_START_Y = 10;
+
 
 CONST CHAR gsz_MY_WINDOW_CLASS[] = "MyWindowClass";
 CONST CHAR gsz_WINDOW_NAME[] = "My First Window";
@@ -36,17 +63,21 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 
 	int screen_width = GetSystemMetrics(SM_CXSCREEN);
 	int screen_height = GetSystemMetrics(SM_CYSCREEN);
-	int window_width = screen_width * .75;
-	int window_height = screen_height * .75;
+	/*int window_width = screen_width * .75;
+	int window_height = screen_height * .75;*/
+
+	int window_width = g_START_X * 3.5 + (g_BUTTON_SIZE + g_INTERVAL) * 5;
+	int window_height = g_START_Y + 20 + (g_BUTTON_SIZE + g_INTERVAL) * 4 + g_INTERVAL * 2 + 55;
 	int start_x = (screen_width - screen_width * .75) / 2;
-	int start_y = screen_height/8;
+	int start_y = screen_height / 8;
 
 	HWND hwnd = CreateWindowEx
 	(
 		NULL, //ExStyle
 		gsz_MY_WINDOW_CLASS, // class name
 		gsz_WINDOW_NAME, // Window name
-		WS_OVERLAPPEDWINDOW,
+		//WS_OVERLAPPEDWINDOW, //dwStyle
+		WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX, //dwStyle
 		start_x, start_y, //Начальная позиция окна
 		window_width, window_height, //Размер окна
 		NULL, //HWND родительского окна. У главного окна нет родительского окна
@@ -76,8 +107,140 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
 	{
-	case WM_CREATE:break;
-	case WM_COMMAND:break;
+	case WM_CREATE:
+	{
+		//Создаем экран калькулятора
+		CreateWindowEx
+		(NULL, "Edit", "",
+			WS_CHILDWINDOW | WS_VISIBLE | WS_BORDER | ES_READONLY | ES_RIGHT,
+			g_START_X, g_START_Y,
+			(g_BUTTON_SIZE + g_INTERVAL) * 5, 20,
+			hwnd,
+			(HMENU)IDC_EDIT,
+			GetModuleHandle(NULL),
+			NULL
+		);
+		//Создаем кнопки:
+		int digit = 1;
+		char sz_digit[] = "0";
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				sz_digit[0] = '0' + digit;
+				CreateWindowEx
+				(
+					NULL, "BUTTON", sz_digit,
+					WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+					g_START_X + (g_BUTTON_SIZE + g_INTERVAL) * j,
+					g_START_Y + g_DISPLAY_HEIGHT + g_INTERVAL + (g_BUTTON_SIZE + g_INTERVAL) * (2 - i),
+					g_BUTTON_SIZE, g_BUTTON_SIZE,
+					hwnd,
+					(HMENU)(IDC_BUTTON_0 + digit),
+					GetModuleHandle(NULL),
+					NULL
+				);
+				digit++;
+			}
+		}
+		CONST CHAR* sz_operation[] = { "+","-", "*","/" };
+		for (int i = 0; i < 4; i++)
+		{
+			CreateWindowEx
+			(
+				NULL, "BUTTON", sz_operation[i],
+				WS_CHILDWINDOW | WS_VISIBLE | BS_PUSHBUTTON,
+				g_START_X + (g_BUTTON_SIZE + g_INTERVAL) * 3,
+				g_START_Y + g_DISPLAY_HEIGHT + g_INTERVAL + (g_BUTTON_SIZE + g_INTERVAL) * i,
+				g_BUTTON_SIZE, g_BUTTON_SIZE,
+				hwnd,
+				(HMENU)(IDC_BUTTON_PLUS + i),
+				GetModuleHandle(NULL),
+				NULL
+			);
+		}
+		CreateWindowEx
+		(
+			NULL, "BUTTON", "0",
+			WS_CHILDWINDOW | WS_VISIBLE | BS_PUSHBUTTON,
+			g_START_X, g_START_Y + g_DISPLAY_HEIGHT + g_INTERVAL + (g_BUTTON_SIZE + g_INTERVAL) * 3,
+			g_BUTTON_SIZE * 2 + g_INTERVAL, g_BUTTON_SIZE,
+			hwnd,
+			(HMENU)IDC_BUTTON_0,
+			GetModuleHandle(NULL),
+			NULL
+		);
+		CreateWindowEx
+		(
+			NULL, "BUTTON", ".",
+			WS_CHILDWINDOW | WS_VISIBLE | BS_PUSHBUTTON,
+			g_START_X + (g_BUTTON_SIZE + g_INTERVAL) * 2,
+			g_START_Y + g_DISPLAY_HEIGHT + g_INTERVAL + (g_BUTTON_SIZE + g_INTERVAL) * 3,
+			g_BUTTON_SIZE, g_BUTTON_SIZE,
+			hwnd,
+			(HMENU)IDC_BUTTON_POINT,
+			GetModuleHandle(NULL),
+			NULL
+		);
+		CreateWindowEx
+		(
+			NULL, "BUTTON", "C",
+			WS_CHILDWINDOW | WS_VISIBLE | BS_PUSHBUTTON,
+			g_START_X + (g_BUTTON_SIZE + g_INTERVAL) * 4,
+			g_START_Y + 20 + g_INTERVAL,
+			g_BUTTON_SIZE, g_BUTTON_SIZE * 2 + g_INTERVAL,
+			hwnd,
+			(HMENU)IDC_BUTTON_CLEAR,
+			GetModuleHandle(NULL),
+			NULL
+		);
+		CreateWindowEx
+		(
+			NULL, "BUTTON", "=",
+			WS_CHILDWINDOW | WS_VISIBLE | BS_PUSHBUTTON,
+			g_START_X + (g_BUTTON_SIZE + g_INTERVAL) * 4,
+			g_START_Y + 20 + g_INTERVAL * 3 + g_BUTTON_SIZE * 2,
+			g_BUTTON_SIZE, g_BUTTON_SIZE * 2 + g_INTERVAL,
+			hwnd,
+			(HMENU)IDC_BUTTON_EQUAL,
+			GetModuleHandle(NULL),
+			NULL
+		);
+	}
+	break;
+	case WM_COMMAND:
+	{
+		CONST INT SIZE = 256;
+		CHAR sz_buffer[SIZE] = {};
+		CHAR sz_digit[] = "0";
+		HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
+		SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+		if (LOWORD(wParam) >= IDC_BUTTON_0 && LOWORD(wParam) <= IDC_BUTTON_9)
+		{
+			sz_digit[0] = LOWORD(wParam) - 952;//получаем ASCII код нажатой цифры
+			strcat(sz_buffer, sz_digit);
+			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		}
+		if (LOWORD(wParam) == IDC_BUTTON_POINT)
+		{
+			if (strchr(sz_buffer, '.'))break;
+			strcat(sz_buffer, ".");
+			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		}
+		if (LOWORD(wParam) == IDC_BUTTON_CLEAR)SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)"");
+	}
+	break;
+	case WM_KEYDOWN:
+	{
+		if (LOWORD(wParam) >= '0' && LOWORD(wParam) <= '9')	SendMessage(hwnd, WM_COMMAND, LOWORD(wParam) + 1000 - '0', 0);
+		//if (LOWORD(wParam) == VK_OEM_PERIOD)	SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_POINT, 0);
+		switch (LOWORD(wParam))
+		{
+		case VK_OEM_PERIOD: SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_POINT, 0); break;
+		case VK_ESCAPE:SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_CLEAR, 0); break;
+		}
+	}
+	break;
 	case WM_SIZE:
 	case WM_MOVE:
 	{
@@ -90,17 +253,17 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			rect.right - rect.left, rect.bottom - rect.top);
 		SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_buffer);
 	}
-		break;
+	break;
 	case WM_DESTROY:PostQuitMessage(0); break;
 	case WM_CLOSE:
-		switch (MessageBox(hwnd, "Вы действительно хотите закрыть окно?", "Вопрос на миллион долларов", MB_YESNO | MB_ICONQUESTION))
+		/*switch (MessageBox(hwnd, "Вы действительно хотите закрыть окно?", "Вопрос на миллион долларов", MB_YESNO | MB_ICONQUESTION))
 		{
-		case IDYES:	
-			MessageBox(hwnd, "Лучше бы двери закрыли)", "Полезная инфа", MB_OK|MB_ICONINFORMATION);
-			DestroyWindow(hwnd); 
+		case IDYES:
+			MessageBox(hwnd, "Лучше бы двери закрыли)", "Полезная инфа", MB_OK | MB_ICONINFORMATION);
+			DestroyWindow(hwnd);
 			break;
 		}
-		break;
+		break;*/
 	default: return DefWindowProc(hwnd, uMsg, wParam, lParam);
 	}
 	return 0;
