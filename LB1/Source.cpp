65e5dc28282960237cc2edf70963e69f481a4a64
Nickshe11 <shelobod@gmail.com>
2023-01-25 16:30:19 +0300

    ListBox

---
+++ LB1/Source.cpp
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <Windows.h>
+#include <stdio.h>
+#include "resource.h"
+
+CONST CHAR* string[] = { "This", "is", "my", "first", "List", "Box" };
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
+	return 0;
+}
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+	{
+		HWND hList = GetDlgItem(hwnd, IDC_LIST1);
+		for (int i = 0; i < sizeof(string) / sizeof(string[0]); i++)
+		{
+			SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)string[i]);
+		}
+	}
+	break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = {};
+			CHAR sz_output_buffer[SIZE] = {};
+			HWND hCombo = GetDlgItem(hwnd, IDC_LIST1);
+			int i = SendMessage(hCombo, LB_GETCURSEL, 0, 0);//Получаем номер выделенного элемента ComboBox
+			SendMessage(hCombo, LB_GETTEXT, i, (LPARAM)sz_buffer);
+			sprintf(sz_output_buffer, "Вы выбрали строку номер %i со значением \"%s\".", i, sz_buffer);
+			MessageBox(hwnd, (LPCSTR)sz_output_buffer, (LPCSTR)"Selecterd item", MB_OK | MB_ICONINFORMATION);
+		}
+		break;
+		case IDCANCEL: EndDialog(hwnd, 0); break;
+		}
+		break;
+	case WM_CLOSE:EndDialog(hwnd, 0);
+	}
+	return FALSE;
+}
